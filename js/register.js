02ab834e24ec4fcd5713d64791d13bf5a5891a9d
emmyxx <emmy.alquie@etu.umontpellier.fr>
2023-06-02 15:45:21 +0200

    répare le register

--- js/register.js
+++ js/register.js
@@ -1,34 +1,37 @@
-document.getElementById("register-form").addEventListener("submit", async function (event) {
+document
+  .getElementById("register-form")
+  .addEventListener("submit", async function (event) {
     event.preventDefault();
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     const messageElement = document.getElementById("message");
-console.log(email, password);
+    console.log(email, password);
     fetch("https://projet-web-back-2wc1.onrender.com/appUsers/register", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            email: email,
-            password: password,
-        }),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
     })
-    .then((response) => response.json())
-    .then((response) => {
-        if (response.message) {
-            messageElement.textContent = "Inscription réussie";
-            setTimeout(function() {
-                messageElement.textContent = "";
-                window.location.href = "index.html";
-            }, 1000);
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response.error);
+        if (!response.error) {
+          messageElement.textContent = "Inscription réussie";
+          setTimeout(function () {
+            messageElement.textContent = "";
+            window.location.href = "index.html";
+          }, 1000);
         } else {
-            messageElement.textContent = "Echec de l'inscription: " + response.error;
+          messageElement.textContent =
+            "Echec de l'inscription: " + response.error;
         }
-    })
-    .catch((error) => {
+      })
+      .catch((error) => {
         console.error("Erreur:", error);
-    });
-});
-
+      });
+  });
